b78b5161ca37229a8623941bd3a2df552740b3c2
Kadam Yogeshwar <yogeshpatil2236@gmail.com>
2025-07-28 22:29:26 +0530

    Add files via upload

---
+++ C module/Tests/Test 1/last2digit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	int no,lasttwodigit;
+	printf("Enter the Number:");
+	scanf("%d",&no);
+	lasttwodigit=no%100;
+	printf("Last two digit :%d",lasttwodigit);
+	
+	
+}
