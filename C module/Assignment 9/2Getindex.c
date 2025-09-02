d4d90690d97fd5b2abbc45c3af55c706d780c9f3
Kadam Yogeshwar <yogeshpatil2236@gmail.com>
2025-09-02 21:45:39 +0530

    Add files via upload

---
+++ C module/Assignment 9/2Getindex.c
@@ -0,0 +1,38 @@
+//2. Search the given number in array.
+#include<stdio.h>
+void GetIndex(int arr[], int );
+void main(){
+	int arr[10];
+	int size=sizeof(arr)/sizeof(int);
+	
+	printf("Enter elements of Array:");
+	for(int i=0;i<size;i++){
+		scanf("%d",&arr[i]);
+	} 
+	printf("Array Displays:\n");
+	for(int i=0;i<size;i++){
+	
+		printf("%d\t",arr[i]);
+	}printf("\n");
+	
+	GetIndex(arr,size);
+		
+}//main ends here
+
+void GetIndex(int arr[], int size){
+		int i,n,found=0;
+	printf("Enter the Number:");
+	scanf("%d",&n);
+		for( i=0;i<size;i++){
+		if(arr[i]==n){
+		
+		printf("%d at %d index\n",n,i);
+	    found=1;}
+		
+	}
+	if(!found)
+	printf("no not found");	
+	
+	
+	
+}
